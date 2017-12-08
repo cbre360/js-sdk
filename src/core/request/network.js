62f38f6dbabf5041d351dc50e5f65829d277179d
Georgi Prodanov <georgi.prodanov@progress.com>
2017-12-08 16:18:54 +0200

    Update datastores and tests.

--- src/core/request/network.js
+++ src/core/request/network.js
@@ -164,7 +164,7 @@ export class KinveyRequest extends NetworkRequest {
     this.trace = options.trace === true;
   }
 
-  static executeShort(options, client, dataOnly = true) {
+  static execute(options, client, dataOnly = true) {
     const o = assign({
       method: RequestMethod.GET,
       authType: AuthType.Session
@@ -179,16 +179,12 @@ export class KinveyRequest extends NetworkRequest {
       });
     }
 
-    let prm = KinveyRequest.execute(o);
+    let prm = new KinveyRequest(o)
+      .execute();
     if (dataOnly) {
       prm = prm.then(r => r.data);
     }
     return prm;
-  }
-
-  static execute(options) {
-    return new KinveyRequest(options)
-      .execute();
   }
 
   get appVersion() {
